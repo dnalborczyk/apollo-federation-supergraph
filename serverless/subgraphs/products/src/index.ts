318d6e3d51eb94ef1bd2b0b3ca62732a1b36b925
Daniel Nalborczyk <dnalborczyk@gmail.com>
2021-09-24 10:08:16 -0400

    refactor: move code into src folder

--- serverless/subgraphs/products/src/index.ts
+++ serverless/subgraphs/products/src/index.ts
@@ -1,30 +1,30 @@
-import { buildSubgraphSchema } from '@apollo/federation'
-import { ApolloServer } from 'apollo-server-lambda'
-import schemaAst from './products.json'
+import { buildSubgraphSchema } from "@apollo/federation";
+import { ApolloServer } from "apollo-server-lambda";
+import schemaAst from "./products.json";
 
 const products = [
   {
-    id: 'apollo-federation',
-    package: '@apollo/federation',
-    sku: 'federation',
-    variation: 'OSS',
+    id: "apollo-federation",
+    package: "@apollo/federation",
+    sku: "federation",
+    variation: "OSS",
   },
   {
-    id: 'apollo-studio',
-    package: '',
-    sku: 'studio',
-    variation: 'platform',
+    id: "apollo-studio",
+    package: "",
+    sku: "studio",
+    variation: "platform",
   },
-]
+];
 
 const resolvers = {
   Query: {
     allProducts: (_, args, context) => {
-      return products
+      return products;
     },
 
     product: (_, args, context) => {
-      return products.find((p) => p.id == args.id)
+      return products.find((p) => p.id == args.id);
     },
   },
 
@@ -33,53 +33,53 @@ const resolvers = {
       if (reference.variation) {
         return {
           id: reference.variation,
-        }
+        };
       }
 
       return {
         id: products.find((p) => p.id == reference.id).variation,
-      }
+      };
     },
 
     dimensions() {
       return {
-        size: '1',
+        size: "1",
         weight: 1,
-      }
+      };
     },
 
     createdBy(reference) {
       return {
-        email: 'support@apollographql.com',
+        email: "support@apollographql.com",
         totalProductsCreated: 1337,
-      }
+      };
     },
 
     __resolveReference: (reference) => {
       if (reference.id) {
-        return products.find((p) => p.id == reference.id)
+        return products.find((p) => p.id == reference.id);
       }
 
       if (reference.sku && reference.package) {
         return products.find(
-          (p) => p.sku == reference.sku && p.package == reference.package,
-        )
+          (p) => p.sku == reference.sku && p.package == reference.package
+        );
       }
 
       return {
-        id: 'rover',
-        package: '@apollo/rover',
+        id: "rover",
+        package: "@apollo/rover",
         ...reference,
-      }
+      };
     },
   },
-}
+};
 
 const server = new ApolloServer({
   schema: buildSubgraphSchema({
     resolvers,
     typeDefs: schemaAst,
   }),
-})
+});
 
-export default server.createHandler()
+export default server.createHandler();
