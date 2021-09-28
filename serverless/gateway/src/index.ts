a7be4b3a3b1dbb0b6382a4c37a1c66746fb2cab7
Daniel Nalborczyk <dnalborczyk@gmail.com>
2021-09-28 14:41:03 -0400

    fix: apigateway url for local development and deployment

--- serverless/gateway/src/index.ts
+++ serverless/gateway/src/index.ts
@@ -1,9 +1,26 @@
-import { ApolloGateway } from '@apollo/gateway'
+import { env } from 'process'
+import { join } from 'path'
+import {
+  ApolloGateway,
+  GraphQLDataSource,
+  RemoteGraphQLDataSource,
+  ServiceEndpointDefinition,
+} from '@apollo/gateway'
 import { ApolloServer } from 'apollo-server-lambda'
 import { print } from 'graphql'
 import schemaAst from 'schema-gateway'
 
 const gateway = new ApolloGateway({
+  buildService(definition: ServiceEndpointDefinition): GraphQLDataSource {
+    const { name } = definition
+
+    return new RemoteGraphQLDataSource({
+      url: env.IS_OFFLINE
+        ? join('http://localhost:4000/dev', name)
+        : join(env.APIGATEWAY_URL as string, name),
+    })
+  },
+
   supergraphSdl: print(schemaAst),
 })
 
